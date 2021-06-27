69bb78b448f7988aaa5f96c57f7f1699d8d9603c
davinash97 <avinashd2904@gmail.com>
2021-06-27 09:31:07 +0530

    Tried to make it more easy

--- shell.sh
+++ shell.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-source shell.fucntions
+source tools/shell.fucntions
 # This file is a part of https://www.github.com/davinash97/funcshell
 # Feel Free to use this file.
 # Just make sure to tag this Repo, And Credits.
 # So that new users can go through the commands.
 # Or use these to implement in their Projects.
 
-print "Hello World!..." "yellow"
-print_logo "banner.txt" "cyan"
+# print "Hello World!..." "yellow"
+# print_logo "banner.txt" "cyan"
+print "This is echo" "grey_bg"
